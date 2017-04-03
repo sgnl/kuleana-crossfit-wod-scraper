4e5b0f2853bbc969d542944da2e3cfa61d78dbf3
sgnl <rayrfarias@gmail.com>
2017-04-02 23:25:46 -1000

    🎉 adds header

--- src/index.js
+++ src/index.js
@@ -7,19 +7,23 @@ const getDate = () => {
   return moment().add(1, 'days').format('YYYY-MM-DD')
 }
 
+const markdownOptions = {
+  converters: [
+    {
+      filter: ['div', 'a'],
+      replacement: content => {
+        return content
+      }
+    }
+  ]
+}
+
 got(`https://crossfitk.sites.zenplanner.com/leaderboard-day.cfm?date=${getDate()}`)
   .then(res => {
     const $ = cheerio.load(res.body)
-    const html = $('.workout').html()
-
-    return toMarkdown(html, { converters: [
-      {
-        filter: ['div', 'a'],
-        replacement: content => {
-          return content
-        }
-      }
-    ]})
+    const html = toMarkdown($('.workout').html(), markdownOptions)
+    const header = toMarkdown($('#idPage h2').html(), markdownOptions)
+    return `# ${header}\n\n${html}`
   })
   .then(markdown => console.log(markdown))
   .catch(err => console.error)
