016d3ed5c77b276ac0f90c66b18b6d069fecdf07
sgnl <rayrfarias@gmail.com>
2017-04-02 23:54:35 -1000

    🎉 📱 💅 SMS and fixes some text formatting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import got from 'got'
 import cheerio from 'cheerio'
 import toMarkdown from 'to-markdown'
 import moment from 'moment'
+import twilio from 'twilio'
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const twilioClient = twilio(accountSid, authToken)
 
 const getDate = () => {
   return moment().add(1, 'days').format('YYYY-MM-DD')
@@ -10,12 +15,29 @@ const getDate = () => {
 const markdownOptions = {
   converters: [
     {
-      filter: ['div', 'a'],
+      // 'div.skillName'
+      filter: node => {
+        return node.classList.contains('skillName')
+      },
+      replacement: content => {
+        return `${content}\n\n`
+      }
+    },
+    {
+      filter: ['a', 'div'],
       replacement: content => {
         return content
       }
     }
   ]
+}
+
+const sendSMS = body => {
+  return twilioClient.messages.create({
+      body,
+      to: '+18083674380',
+      from: '+18082011211',
+  })
 }
 
 got(`https://crossfitk.sites.zenplanner.com/leaderboard-day.cfm?date=${getDate()}`)
@@ -26,5 +48,7 @@ got(`https://crossfitk.sites.zenplanner.com/leaderboard-day.cfm?date=${getDate()
     return `# ${header}\n\n${html}`
   })
   .then(markdown => console.log(markdown))
+  // .then(sendSMS)
+  .then(() => console.log('SMS success?!'))
   .catch(err => console.error)
 
